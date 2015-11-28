03326d59797446fa86d7c8517c132a1733927ff9
brydenfogelman <bryden1995@gmail.com>
2015-11-28 11:35:21 -0800

    created functionality for button click on main activity

---
+++ app/src/main/java/com/brydenfogelman/startupweekend/SignUp.java
@@ -0,0 +1,14 @@
+package com.brydenfogelman.startupweekend;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SignUp extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
