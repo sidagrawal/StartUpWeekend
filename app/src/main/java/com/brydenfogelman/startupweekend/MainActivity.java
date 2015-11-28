76d43630995e2adb919200cd9e9c2f26f1a7e0ac
brydenfogelman <bryden1995@gmail.com>
2015-11-28 11:49:52 -0800

    push didn't go through, re-pushing

--- app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 
     @Override
