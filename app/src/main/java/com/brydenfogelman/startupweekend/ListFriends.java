8bc727d0a0732ce92279ce3192cca7aa89bd7e06
brydenfogelman <bryden1995@gmail.com>
2015-11-28 21:46:37 -0800

    changed so it loads to main activity

--- app/src/main/java/com/brydenfogelman/startupweekend/ListFriends.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/ListFriends.java
@@ -1,30 +1,14 @@
 package com.brydenfogelman.startupweekend;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class ListFriends extends ActionBarActivity {
 
-    ListView lv;
-    Context context;
-
-    ArrayList prgmName;
-    public static int [] avatars={R.drawable.blank_profile_photo,R.drawable.blank_profile_photo,R.drawable.blank_profile_photo,R.drawable.blank_profile_photo,};
-    public static String [] screenNames={"Bryden","Jay","Sid","Tunir"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        context=this;
-
-        lv=(ListView) findViewById(R.id.listView);
-        lv.setAdapter(new CustomAdapter(this, screenNames, avatars));
-
+        setContentView(R.layout.activity_list_friends);
     }
 
 }
