7872116cfdfaa27c241e4fe7addbc6a11578d407
Jayvardhana Agrawal <jay.agrawal31@gmail.com>
2015-11-28 19:19:09 -0800

    starting the trails list activity from the filter activity with the necassary info

--- app/src/main/java/com/brydenfogelman/startupweekend/Find_trails.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/Find_trails.java
@@ -1,10 +1,13 @@
 package com.brydenfogelman.startupweekend;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,7 +17,8 @@ public class Find_trails extends ActionBarActivity {
     SeekBar hike_time;
     TextView driving_time_text;
     TextView hike_time_text;
-
+    Spinner difficulty;
+    Spinner other_locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class Find_trails extends ActionBarActivity {
         hike_time = (SeekBar)findViewById(R.id.trailTimeBar);
         driving_time_text = (TextView)findViewById(R.id.driving_time);
         hike_time_text = (TextView)findViewById(R.id.hike_time);
+        difficulty = (Spinner)findViewById(R.id.difficulty);
+        other_locations = (Spinner)findViewById(R.id.other_locations);
 
         driving_time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChanged = 0;
@@ -54,6 +60,15 @@ public class Find_trails extends ActionBarActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+    }
 
+    public void go(View view){
+        Intent intent = new Intent(this, Trail_List.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("region", other_locations.getSelectedItem().toString());
+        bundle.putString("difficulty", difficulty.getSelectedItem().toString());
+        bundle.putString("hike_time", hike_time_text.getText().toString());
+        bundle.putString("drive_time", driving_time_text.getText().toString());
+        startActivity(intent, bundle);
     }
 }
