e4bc2967b151c0181c14fc68988b9538f89a38da
Siddhant Agrawal <sidagrawal94@gmail.com>
2015-11-29 10:59:28 -0800

    commented out the action bar in ain acitivty

--- app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    @Override
+   /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -36,7 +36,7 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     public void onClickLogin(View view) {
         Intent intent = new Intent(this, Login.class);
