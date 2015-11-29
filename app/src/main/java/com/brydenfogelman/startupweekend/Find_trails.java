ab41868d4719b6dfdb7499c37450f9ef9977cf85
Siddhant Agrawal <sidagrawal94@gmail.com>
2015-11-29 12:45:51 -0800

    Created action bar in Trail_find and resolved merge conflicts

--- app/src/main/java/com/brydenfogelman/startupweekend/Find_trails.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/Find_trails.java
@@ -1,23 +1,16 @@
 package com.brydenfogelman.startupweekend;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
-import android.view.Menu;
-import android.view.View;
 import android.view.MenuItem;
 
-public class Find_trails extends Activity {
+public class Find_trails extends ActionBarActivity {
 
     SeekBar driving_time;
     SeekBar hike_time;
@@ -54,10 +47,12 @@ public class Find_trails extends Activity {
         });
 
 
+
+
         hike_time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChanged = 0;
 
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 hike_time_text.setText(Integer.toString(progress));
             }
 
@@ -68,6 +63,31 @@ public class Find_trails extends Activity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if(id == R.id.action_profile){
+            Intent intent = new Intent(this, Profile.class);
+            startActivity(intent);
+        } else if(id == R.id.action_friends){
+            Intent intent = new Intent(this, ListFriends.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void go(View view){
--- app/src/main/java/com/brydenfogelman/startupweekend/Profile.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/Profile.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-public class friends extends Activity {
+public class Profile extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
