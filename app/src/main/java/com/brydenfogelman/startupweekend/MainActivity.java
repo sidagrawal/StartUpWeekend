fe14da46e313816bc32b79cc4aaa652cf46a1f28
brydenfogelman <bryden1995@gmail.com>
2015-11-28 20:40:38 -0800

    added a lot of views for the friend functionality
    also working adding friends to be loaded

--- app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_friends);
     }
 
     @Override
