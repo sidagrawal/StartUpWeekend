0673766a646f3740b1046e795d06f708e2f6302f
brydenfogelman <bryden1995@gmail.com>
2015-11-29 13:36:17 -0800

    not much

--- app/src/main/java/com/brydenfogelman/startupweekend/FindTrails.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/FindTrails.java
@@ -1,6 +1,10 @@
 package com.brydenfogelman.startupweekend;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
