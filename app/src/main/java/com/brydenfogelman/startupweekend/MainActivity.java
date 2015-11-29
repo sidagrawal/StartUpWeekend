33bf8e3db167787efaf94f879e030290a16f327b
brydenfogelman <bryden1995@gmail.com>
2015-11-28 23:22:08 -0800

    lots of changes, forgot what I did, played around with a material theme

--- app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_friends);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
