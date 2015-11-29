c02c4869c4e14c4371b9f4e65e072275129bc7ae
Jayvardhana Agrawal <jay.agrawal31@gmail.com>
2015-11-28 20:04:41 -0800

    got rid of action bar in main activity

--- app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.brydenfogelman.startupweekend;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -7,7 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/brydenfogelman/startupweekend/Trail_List.java
+++ app/src/main/java/com/brydenfogelman/startupweekend/Trail_List.java
@@ -1,15 +1,73 @@
 package com.brydenfogelman.startupweekend;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Trail_List extends ActionBarActivity {
+    String region;
+    String difficulty;
+    String hike_time;
+    String drive_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trail__list);
-        String region = savedInstanceState.getString("region");
+        region = savedInstanceState.getString("region");
+        difficulty = savedInstanceState.getString("difficulty");
+        hike_time = savedInstanceState.getString("hike_time");
+        drive_time = savedInstanceState.getString("drive_time");
+
+
+    }
+}
+
+class phpGetTrails extends AsyncTask<String, Void, String> {
+    private Context context;
+
+    public phpGetTrails(Context context) {
+        this.context = context;
     }
 
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            String region = params[0].trim();
+            String difficulty = params[1].trim();
+            String hike_time = params[2].trim();
+            String drive_time = params[3].trim();
+
+            String link = "http://159.203.66.71/Startup%20Weekend/get_trails.php?region=" +
+                    region + "&difficulty="+ difficulty + "&hike_time=" + hike_time  + "&drive_time=" +
+                    drive_time;
+            Log.d("link", link);
+
+            URL url = new URL(link);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "");
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            in.close();
+            Log.d("result", sb.toString());
+            return sb.toString();
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
 }
